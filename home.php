ea5c42c6f443eff465a37c4e14c33e7425aafde3
unknown <zlubiz8@gmail.com>
2022-10-14 22:58:11 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styling/style.css">
+    <title>Welcome | Zul's Porto</title>
+
+    <script src="https://kit.fontawesome.com/5b7e08bb94.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="icon-container">
+            <div class="icon">
+                <a href="https://api.whatsapp.com/send?phone=6285161124894"><i class="fa-brands fa-whatsapp"></i><br></a>
+                <a href="https://instagram.com/zulfikaralwilubis"><i class="fa-brands fa-instagram"></i><br></a>
+                <a href="https://tiktok.com/@zeday_"><i class="fa-brands fa-tiktok"></i><br></a>
+                <a href="mailto: zlubiz8@gmail.com"><i class="fa-regular fa-envelope"></i><br></a>
+                <div class="bar">․</div>
+            </div>
+        </div>
+    </div>
+    <div class="navbar">
+        <div class="web-icon">
+            <i class="fa fa-wave-square"></i>
+        </div>
+        <div class="web-title">
+            Welcome to my website
+        </div>
+    </div>
+    <div class="content-container">
+        <span class="greeting-text">👋 hai, my name is</span>
+        <div class="greeting-title">
+            Zulfikar Alwi<br>
+            I build things for anything.
+        </div>
+        <div class="greeting-description">
+            I am a student from the faculty of computer science engineering, I am 22 years old,<br> I have an interest in the world of coding, <br>especially in coding an application for mobile. <br>there are some of my works on <a href="">my desk</a> 
+        </div>
+        <div class="mydesk-button-container">
+            <a href=""><button class="mydesk-button"> See My Desk</button></a>
+        </div>
+    </div>
+</body>
+</html>
